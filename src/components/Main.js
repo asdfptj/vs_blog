78be4a6823ff07b18a5c9ec400bb22cb2e59967f
asdfptj <taejunb70@gmail.com>
2022-11-04 09:34:04 +0900

    commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { VscFiles, VscSearch } from "react-icons/vsc";
+import Accordion from './Accordion';
 
 
 const listArr = [
@@ -24,35 +25,55 @@ function Main() {
                     <IconWrap
                         selected={selected === index}
                         onClick={() => {
-                            setSelected(index)
+                            setSelected(selected === index ? null : index)
                         }}
                     >
                         {one.icon}
                     </IconWrap>
                 ))}
             </LeftBar>
+            <LeftContent>
+                <p>{listArr[selected]?.path}</p>
+                <Accordion title="OPEN POSTS">내용ㅋ</Accordion>
+            </LeftContent>
         </Wrap>
     );
 }
 
 export default Main;
 
-const Wrap = styled.div`
-    height: 100vh;
-    background-color: #252526;
-`;
-const LeftBar = styled.div`
-    width: 50px;
-    height: 100%;
-    background-color: #333333;
-`
 const IconWrap = styled.div`
     display: flex;
     justify-content: center;
     padding: 10px 0;
     cursor: pointer;
 
+
+    border-left: ${({ selected }) => (selected ? 2 : 0)}px solid white;
+
     > svg {
         color: ${({ selected }) => (selected ? "white" : "#7a7a7a")};
     }
-`;
+    `;
+const Wrap = styled.div`
+    display: flex;
+    height: 100vh;
+    background-color: white;
+        `;
+const LeftBar = styled.div`
+    width: 50px;
+    height: 100%;
+    background-color: #333333;
+        `
+const LeftContent = styled.div`
+    width: 320px;
+    height: 100%;
+    background-color: #252526;
+    
+
+    > p {
+        padding: 15px 0 0 10px;
+        color: #7a7a7a;
+    }
+
+    `;
