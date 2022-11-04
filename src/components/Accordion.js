f5c21cc00ee5b115f2b27bc0b827b58cc69b4bae
asdfptj <taejunb70@gmail.com>
2022-11-04 10:10:49 +0900

    commit

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { VscChevronRight } from "react-icons/vsc";
 
-function Accordion() {
-    return (
-        <div>Accordion</div>
-    )
+function Accordion({ title, children }) {
+  const [expanded, setExpanded] = useState(false);
+  console.log(title);
+
+  return (
+    <>
+      <AccordionWrap
+        onClick={() => {
+          setExpanded(!expanded);
+        }}
+      >
+        <VscChevronRight />
+        <span>{title}</span>
+      </AccordionWrap>
+      {expanded && <AccordionContentWrap> {children}</AccordionContentWrap>}
+    </>
+  );
 }
 
-export default Accordion
+export default Accordion;
+
+const AccordionWrap = styled.div`
+  display: flex;
+  align-items: center;
+  color: white;
+  font-weight: bold;
+  font-size: 0.8rem;
+  padding-left: 5px;
+
+  > span {
+    padding-left: 5px;
+  }
+`;
+
+const AccordionContentWrap = styled.div``;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,79 +1,94 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import { VscFiles, VscSearch } from "react-icons/vsc";
-import Accordion from './Accordion';
-
+import React, { useState } from "react";
+import styled from "styled-components";
+import Accordion from "./Accordion";
+import {
+  VscFile,
+  VscSearch,
+  VscSourceControl,
+  VscDebugAlt,
+  VscExtensions,
+} from "react-icons/vsc";
 
 const listArr = [
-    {
-        icon: <VscFiles />,
-        path: "post",
-    },
-    {
-        icon: <VscSearch />,
-        path: "search",
-    },
-]
-
+  {
+    icon: <VscFile size={22.4} />,
+    path: "EXPLORER",
+  },
+  {
+    icon: <VscSearch size={22.4} />,
+    path: "SEARCH",
+  },
+  {
+    icon: <VscSourceControl size={22.4} />,
+    path: "POSTING LOG",
+  },
+  {
+    icon: <VscDebugAlt size={22.4} />,
+    path: "RUN AND DEBUG",
+  },
+  {
+    icon: <VscExtensions size={22.4} />,
+    path: "EXTENSIONS",
+  },
+];
 function Main() {
-    const [selected, setSelected] = useState(null)
+  const [selected, setSelected] = useState(null);
 
-    return (
-        <Wrap>
-            <LeftBar>
-                {listArr.map((one, index) => (
-                    <IconWrap
-                        selected={selected === index}
-                        onClick={() => {
-                            setSelected(selected === index ? null : index)
-                        }}
-                    >
-                        {one.icon}
-                    </IconWrap>
-                ))}
-            </LeftBar>
-            <LeftContent>
-                <p>{listArr[selected]?.path}</p>
-                <Accordion title="OPEN POSTS">내용ㅋ</Accordion>
-            </LeftContent>
-        </Wrap>
-    );
+  return (
+    <Wrap>
+      <LeftBar>
+        {listArr.map((one, index) => (
+          <IconWrap
+            selected={selected === index}
+            onClick={() => {
+              setSelected(selected === index ? null : index);
+            }}
+          >
+            {one.icon}
+          </IconWrap>
+        ))}
+      </LeftBar>
+      {selected !== null && listArr[selected] && (
+        <LeftContent>
+          <p>{listArr[selected]?.path}</p>
+          <Accordion title="OPEN POSTS">내용ㅋ</Accordion>
+        </LeftContent>
+      )}
+    </Wrap>
+  );
 }
 
 export default Main;
 
 const IconWrap = styled.div`
-    display: flex;
-    justify-content: center;
-    padding: 10px 0;
-    cursor: pointer;
+  display: flex;
+  justify-content: center;
+  padding: 10px 0;
+  cursor: pointer;
 
+  border-left: ${({ selected }) => (selected ? 2 : 0)}px solid white;
 
-    border-left: ${({ selected }) => (selected ? 2 : 0)}px solid white;
-
-    > svg {
-        color: ${({ selected }) => (selected ? "white" : "#7a7a7a")};
-    }
-    `;
+  > svg {
+    color: ${({ selected }) => (selected ? "white" : "#7a7a7a")};
+  }
+`;
 const Wrap = styled.div`
-    display: flex;
-    height: 100vh;
-    background-color: white;
-        `;
+  display: flex;
+  height: 100vh;
+  background-color: white;
+`;
 const LeftBar = styled.div`
-    width: 50px;
-    height: 100%;
-    background-color: #333333;
-        `
+  width: 50px;
+  height: 100%;
+  background-color: #333333;
+`;
 const LeftContent = styled.div`
-    width: 320px;
-    height: 100%;
-    background-color: #252526;
-    
+  width: 320px;
+  height: 100%;
+  background-color: #252526;
 
-    > p {
-        padding: 15px 0 0 10px;
-        color: #7a7a7a;
-    }
-
-    `;
+  > p {
+    padding: 15px 0 0 10px;
+    color: #7a7a7a;
+  }
+`;
